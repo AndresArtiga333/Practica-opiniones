1fd77f33e76caf7f65f8eb217b942d60985f2a91
AndresArtiga333 <andresaartiga057@gmail.com>
2025-02-19 21:28:28 -0600

    Se agrego lo basico para inicial el controlador de publicaciones

---
+++ src/publicaciones/publicaciones.model.js
@@ -0,0 +1,25 @@
+import { Schema, model } from "mongoose";
+ 
+const publicacionesSchema = Schema({
+    titulo:{
+        type: String,
+        required: [true, "El titulo es requerido"],
+        maxLength: [15, "El titulo no puede pasar de 15 caracteres"]
+    },
+    categoria:{
+        type: Schema.ObjectId,
+        ref: 'Categoria',
+        required: true,
+    },
+    texto:{
+        type: String,
+        required: [true, "El texto es necesario"],
+        maxLength: [100, "El texto no puede pasar los 100 caracteres"]
+    },
+    status:{
+        type: Boolean,
+        default: true
+    }
+})
+
+export default model("Publicaciones", publicacionesSchema)
